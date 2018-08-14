6de9cbb9dd7ecf5252b7723896481bc5e2a38828
Dnice916 <39569871+Dnice916@users.noreply.github.com>
2018-08-14 10:30:04 -0700

    Revert "Migrate user profiles to a new domain"
    This reverts commit 1d699707910a19d601b44b3acd93502b5a6c81e0.

--- UPN Update.ps1
+++
@@ -1,9 +0,0 @@
-Import-Module ActiveDirectory
-$oldSuffix = "pnl.com"
-$newSuffix = "petenetlive.com"
-$ou = "OU=Test,DC=pnl,DC=com"
-$server = "DC-01"
-Get-ADUser -SearchBase $ou -filter * | ForEach-Object {
-$newUpn = $_.UserPrincipalName.Replace($oldSuffix,$newSuffix)
-$_ | Set-ADUser -server $server -UserPrincipalName $newUpn
-}
